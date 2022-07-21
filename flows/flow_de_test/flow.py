11cda0a831c2eeba7b61d543d28b12fbe3420b73
gegedenice <geraldine.geoffroy@univ-cotedazur.fr>
2022-07-21 14:35:37 +0200

    test github storage

--- flows/flow_de_test/flow.py
+++ flows/flow_de_test/flow.py
@@ -10,18 +10,17 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 import json
-import re
 from datetime import datetime
+import re
 
 FLOW_NAME = "flow-de-test"
+FLOW_PATH = "flows/flow_de_test"
 storage = Local()
 storage_github = GitHub(
     repo="azur-scd/prefect-flows",                            # name of repo
-    path="flows/flow_de_test/flow.py",                    # location of flow file in repo
+    path=f"{FLOW_PATH}/flow.py",                    # location of flow file in repo
     #access_token_secret="prefect"   # name of personal access token secret
 )
-
-FLOW_PATH = "flows/flow_de_test"
 
 def  scrapping_oai_sets_dewey():
     url = "https://www.theses.fr/schemas/tef/recommandation/oai_sets.html"
@@ -41,30 +40,12 @@ def  scrapping_oai_sets_dewey():
     df['main_domain'] = df['code'].apply(lambda x: 'Sciences, Technologies, Santé' if ((str(x[4]) == "5") | (str(x[4]) == "6") | (str(x[4:7]) == "004")) else 'Lettres, sciences Humaines et Sociales')
     return df
 
-def send_notification(obj, old_state, new_state):  
-    now = datetime.now()
-    msg = "{0} : new_state {1} starting at {2}".format(obj,new_state,now.strftime("%Y-%m-%d %H:%M:%S"))
-    #requests.post("http://localhost:5000", json={"data": str(obj) +" : "+str(new_state.message)})
-    requests.post("http://localhost:5000", json.dumps({'data': msg}))
-    return new_state
-
 #@task(log_stdout=True, name="generate_data", state_handlers=[send_notification])
-
-@task(log_stdout=True, name="load_data")
-def load_data() -> pd.DataFrame :
-    df = pd.read_csv(f"{FLOW_PATH}/data/theses_test_processed.csv",sep=",", encoding="utf-8")
-    print(df.shape)
-    return df
-
-@task(log_stdout=True, name="save_data")
-def save_data(df) -> pd.DataFrame:
-    df.to_csv(f"{FLOW_PATH}/data/theses_test_processed_2.csv", index=False, encoding='utf8')
-    return df
 
 @task(log_stdout=True, name="load_oai_data")
 def load_oai_data() -> pd.DataFrame :
     df = scrapping_oai_sets_dewey()
-    df.to_csv(f"{FLOW_PATH}/data/oai_sets.csv", index=False, encoding='utf8')
+    df.to_csv("oai_sets.csv", index=False, encoding='utf8')
     return df
 
 with Flow(name=FLOW_NAME) as flow:
@@ -73,13 +54,11 @@ with Flow(name=FLOW_NAME) as flow:
 
 flow.register(project_name="projet_de_test")
 flow.storage=storage_github
-flow.run_config=LocalRun(labels=["dev"])
+flow.run_config=LocalRun()
 flow.run()
 
-# Prefect cloud
+# Prefect cloud (à la racine du root folder, sinon idem suaf prefect agent start -p ~/flows/flow_de_test)
 ## prefect backend cloud
 ## prefect auth login --key pcu_lcwhwE2WmL4tPYNaww47GDUuYWWkUZ3pQPKc
 ## prefect agent local start
 # Pour enregister le flow dans Prefect Cloud : prefect register -p flows/my_flow.py --project MyProject
-
-# prefect agent start -p ~/Developer/prefect/examples/tutorial
