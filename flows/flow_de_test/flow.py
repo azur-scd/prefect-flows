34b3989c65abe5c3d2144958ade8609c3a238016
gegedenice <geraldine.geoffroy@univ-cotedazur.fr>
2022-07-21 10:36:34 +0200

    Update flow.py

--- flows/flow_de_test/flow.py
+++ flows/flow_de_test/flow.py
@@ -10,19 +10,37 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 import json
-import random
+import re
 from datetime import datetime
-import functions as fn
+import flow_de_test.functions as fn
 
 FLOW_NAME = "flow-de-test"
 storage = Local()
 storage_github = GitHub(
     repo="azur-scd/prefect-flows",                            # name of repo
     path="flows/flow_de_test/flow.py",                    # location of flow file in repo
-    access_token_secret="prefect"   # name of personal access token secret
+    #access_token_secret="prefect"   # name of personal access token secret
 )
 
 FLOW_PATH = "flows/flow_de_test"
+
+def  scrapping_oai_sets_dewey():
+    url = "https://www.theses.fr/schemas/tef/recommandation/oai_sets.html"
+    resp = requests.get(url).text  # ou f = http.request('GET', url).data
+    soup = BeautifulSoup(resp, features="lxml")
+    oai_list = []
+    for row in soup.findAll("table")[0].findAll("tr"):
+        label = re.sub('<!--.*-->|\r|\n', '', str(row.findAll("td")[0].get_text(strip=True)), flags=re.DOTALL)
+        label = re.sub('\s{2,}|&nbsp;', ' ', label)
+        oai_list.append(
+            {
+                "label": label,
+                "code": row.findAll("td")[1].get_text(strip=True),
+            }
+        )
+    df = pd.DataFrame(oai_list[1:])
+    df['main_domain'] = df['code'].apply(lambda x: 'Sciences, Technologies, Santé' if ((str(x[4]) == "5") | (str(x[4]) == "6") | (str(x[4:7]) == "004")) else 'Lettres, sciences Humaines et Sociales')
+    return df
 
 def send_notification(obj, old_state, new_state):  
     now = datetime.now()
@@ -64,3 +82,5 @@ flow.run()
 ## prefect auth login --key pcu_lcwhwE2WmL4tPYNaww47GDUuYWWkUZ3pQPKc
 ## prefect agent local start
 # Pour enregister le flow dans Prefect Cloud : prefect register -p flows/my_flow.py --project MyProject
+
+# prefect agent start -p ~/Developer/prefect/examples/tutorial
